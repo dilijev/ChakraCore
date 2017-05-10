2368d7234eff4c6b6076939d4660cdb05b1e4cd1
Doug Ilijev <doilij@microsoft.com>
2017-05-10 14:12:20 -0700

    WIP

--- tools/Unicode/CaseInsensitive/build.bat
+++ tools/Unicode/CaseInsensitive/build.bat
@@ -5,3 +5,6 @@ REM npm install -g typings
 REM typings install --global env~node
 
 tsc mappings.ts -target es6 -module commonjs
+
+REM node mappings.js ../UCD/UnicodeData-8.0.0.txt ../UCD/CaseFolding-8.0.0.txt ./out/mappings-8.0.0.txt >NUL
+REM node mappings.js ../UCD/UnicodeData-9.0.0.txt ../UCD/CaseFolding-9.0.0.txt ./out/mappings-9.0.0.txt >NUL
